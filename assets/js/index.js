ac44e44cc9f1309349882588ac1c514b37629683
ArtyomHloba <hlobartyom@gmail.com>
2024-05-04 21:47:09 +0300

    2) Створіть дві змінні c та d. Виведіть у консоль результат ділення першого на друге.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,8 @@ const a = 5;
 const b = 10;
 const multiplication = a * b;
 console.log('Множення:', multiplication);
+
+const c = 10;
+const d = 2;
+const division = c / d;
+console.log('Ділення:', division);
