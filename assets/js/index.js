f0054a5926e104ce5af2e93050244da63017f20c
ArtyomHloba <hlobartyom@gmail.com>
2024-05-05 11:18:06 +0300

    4) Визначте змінні зі значеннями 11, true, java script, 100 і виведіть їх в консоль

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,24 @@
+// 1
 const a = 5;
 const b = 10;
 const multiplication = a * b;
 console.log('Множення:', multiplication);
 
+// 2
 const c = 10;
 const d = 2;
 const division = c / d;
 console.log('Ділення:', division);
 
+// 3
 const e = 124;
 const f = 125;
 const addition = e + f;
 console.log('Додавання:', addition);
+
+// 4
+const number = 11;
+const choice = true;
+const promLanguage = 'java script';
+const bigNumber = '100';
+console.log(number, choice, promLanguage, bigNumber);
