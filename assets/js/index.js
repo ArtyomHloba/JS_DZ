d1b505166c8799b6159f0dc70f5698742718dff1
ArtyomHloba <hlobartyom@gmail.com>
2024-05-06 11:00:08 +0300

    3) Перевести кількість хвилин на секунди та вивести результат. Число хвилин запитувати у користувача.

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,37 +4,40 @@
 const a = 5;
 const b = 10;
 const multiplication = a * b;
-console.log('Множення:', multiplication);
+// console.log('Множення:', multiplication);
 
 // 2
 const c = 10;
 const d = 2;
 const division = c / d;
-console.log('Ділення:', division);
+// console.log('Ділення:', division);
 
 // 3
 const e = 124;
 const f = 125;
 const addition = e + f;
-console.log('Додавання:', addition);
+// console.log('Додавання:', addition);
 
 // 4
 const number = 11;
 const choice = true;
 const promLanguage = 'java script';
 const bigNumber = '100';
-console.log(number, choice, promLanguage, bigNumber);
+// console.log(number, choice, promLanguage, bigNumber);
 
 
 // Завдання на введення/виведення
 
-
 // 1
-const number1 = prompt('Input number:')
-console.log(number1**2);
+// const number1 = prompt('Input number:')
+// console.log(number1**2);
 
 // 2
-const number2 = +prompt('Input first number:');
-const number3 = +prompt('Input second number:');
-console.log((number2 + number3) / 2);
+// const number2 = +prompt('Input first number:');
+// const number3 = +prompt('Input second number:');
+// console.log((number2 + number3) / 2);
 
+// 3
+
+const minute = +prompt('Введіть кількість хвилин:')
+console.log(minute * 60, 'секунд');
