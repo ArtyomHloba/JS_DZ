ca653c1eccf809a5a339e604ab3a7e76a7424dff
ArtyomHloba <hlobartyom@gmail.com>
2024-05-06 10:37:16 +0300

    1) Запитати число у користувача, піднести його до квадрату і вивести результат.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+// Завдання на змінні:
+
 // 1
 const a = 5;
 const b = 10;
@@ -22,3 +24,11 @@ const choice = true;
 const promLanguage = 'java script';
 const bigNumber = '100';
 console.log(number, choice, promLanguage, bigNumber);
+
+
+// Завдання на введення/виведення
+
+
+// 1
+const number1 = prompt('Input number:')
+console.log(number1**2);
