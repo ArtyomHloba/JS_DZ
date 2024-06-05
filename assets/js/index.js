c16b79f7377ee0c4284cd9b3dbd2ac530951ddb1
ArtyomHloba <hlobartyom@gmail.com>
2024-06-05 12:32:21 +0300

    Classes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,86 @@
-function counter(number) {
-    let count = 0;
-  
-    function inc() {
-      return count += number;
+class Post{
+    constructor(id, postName, author, text, date, likes, img, hashtag){
+        this._id = id;
+        this._postName = postName;
+        this._author = author;
+        this._text = text;
+        this._date = date;
+        this._likes = likes;
+        this._img = img;
+        this._hashtags = [];
+        this._allowedHashtags = [`#cat #kitty #fullstack #education #tech #coding`];
+    };
+
+    newText(text){
+        this._text = text;
     }
-  
-    return inc;
-  }
-  
-  const count1 = counter(5);
-  console.log(count1());
-  console.log(count1());
-  console.log(count1());
-  console.log(count1());
-  
-  function counter(n, e) {
-    let count = n;
-  
-    function inc() {
-      return count += e;
+
+    plusLike(){
+        this._likes += 1;
     }
-  
-    return inc;
-  }
-  
-  const count1 = counter(+prompt("Введіть початкове число"), +prompt("введіть крок лічильник"));
-  console.log(count1());
-  console.log(count1());
-  console.log(count1());
-  console.log(count1());
+
+    minesLike(){
+        this._likes -= 1;
+    }
+
+    addHashtag(hashtag) {
+        if (this._hashtags.length >= 6) {
+            console.log("Cannot add more than 6 hashtags.");
+        } else if (!this._allowedHashtags.includes(hashtag)) {
+            console.log(`Hashtag '${hashtag}' is not allowed.`);
+        } else if (this._hashtags.includes(hashtag)) {
+            console.log(`Hashtag '${hashtag}' is already added.`);
+        } else {
+            this._hashtags.push(hashtag);
+        }
+    }
+
+    getHashtags() {
+        return this._hashtags;
+    }
+
+    render(){
+        document.write(`
+            <article class="post-cont">
+                <img class="post-photo" src="./assets/img/cat.jpg" alt="cat">
+                <h3 class="post-name">${this._postName}</h3>
+                <p class="autor">${this._author}</p>
+                <p class="post-text">${this._text}</p>
+                <p class="post-date">${this._date}</p>
+                <p class="post-likes">${this._likes} <span class="heart"><i class="fa-solid fa-heart"></i></span> <span class="share"><i class="fa-solid fa-share"></i></span></p>
+                <p class="post-heshtag">${this._allowedHashtags}</p>
+            </article>
+        `);
+    };
+}
+
+const post1 = new Post(
+    1,
+    "Kitty",
+    "Animal Planet",
+    "Fat cat jumping on the bed",
+    2022,
+    100,
+);
+
+console.log(post1);
+
+// змінити текст поста на інший.
+post1.newText("Small cat smile)");
+
+//збільшити кількість вподобайок на 1 
+// зменшити кількість вподобайок на 1.
+post1.plusLike();
+post1.minesLike();
+
+// render для отримання розмітки для посту 
+// (*в методі деструктуризувати інформацію з this).
+
+post1.render();
+
+post1.addHashtag(`javascript`);
+post1.addHashtag(`fullstack`);
+post1.addHashtag(`education`);
 
 
 
