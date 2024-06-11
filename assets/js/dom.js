3cb2dd0509ed3886e2f4701c2f75b5bfe66dd27c
ArtyomHloba <hlobartyom@gmail.com>
2024-06-11 11:19:32 +0300

    Add DOM

---
+++ assets/js/dom.js
@@ -0,0 +1,54 @@
+// const changeTextBtn = document.querySelector('#renderBtn')
+
+// function eventText(e){
+//     changeTextBtn.textContent = 'Змінений текст';
+//     changeTextBtn.style.backgroundColor = "blue";
+//     changeTextBtn.style.color = "white";
+// }
+
+// changeTextBtn.onclick = eventText;
+
+// const imgCat1 = document.querySelector('#imgCat')
+// imgCat1.style.width = "300px";
+
+// imgCat1.onmouseenter = e => {
+//     imgCat1.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5TZSSagPJ9vFKl-fCAFp1b3e3MP0ysmnKCw&s";
+//   };
+  
+//   imgCat1.onmouseleave = e => {
+//     imgCat1.src = "./assets/img/cat.jpg";
+//   };
+
+
+  const user = {
+    firstName: 'Test',
+    lastName: 'Testovych',
+    profilePhoto: 'https://images.pexels.com/photos/4902634/pexels-photo-4902634.jpeg',
+    birthday: new Date('2000-05-16'),
+    nickname: 'super dev',
+    likesCount: 10,
+}
+
+
+document.querySelector('.bnt-render').addEventListener('click', () =>{
+  const article = document.querySelector('.render-card')
+
+  article.innerHTML = `
+    <img class="user-photo" src="${user.profilePhoto}" alt="Profile Photo">
+      <p>Ім'я: ${user.firstName}</p>
+      <p>Прізвище: ${user.lastName}</p>
+      <p>Нікнейм: ${user.nickname}</p>
+      <p>Дата народження: ${user.birthday.toLocaleDateString('uk-UA')}</p>
+      <p>Кількість лайків: ${user.likesCount} <span class="heart"><i class="fa-solid fa-heart"></i></span></p> 
+`;
+
+  const heart = document.querySelector('.heart');
+  if(heart){
+  heart.addEventListener('click', () => {
+    heart.style.color = 'red';
+  });
+  };
+});
+
+
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -69,95 +69,95 @@
 //   console.log(count1());
 
 
-const ALLOWED_HASHTAGS = [
-    '#cat', 
-    '#kitty', 
-    '#fullstack', 
-    '#education', 
-    '#tech', 
-    '#coding',
-];
-class Post{
-    constructor(id, postName, author, text, date, likes, img){
-        this._id = id;
-        this._postName = postName;
-        this._author = author;
-        this._text = text;
-        this._date = date;
-        this._likes = likes;
-        this._img = img;
-        this._hashtags = [];
-    };
+// const ALLOWED_HASHTAGS = [
+//     '#cat', 
+//     '#kitty', 
+//     '#fullstack', 
+//     '#education', 
+//     '#tech', 
+//     '#coding',
+// ];
+// class Post{
+//     constructor(id, postName, author, text, date, likes, img){
+//         this._id = id;
+//         this._postName = postName;
+//         this._author = author;
+//         this._text = text;
+//         this._date = date;
+//         this._likes = likes;
+//         this._img = img;
+//         this._hashtags = [];
+//     };
 
-    newText(text){
-        this._text = text;
-    }
+//     newText(text){
+//         this._text = text;
+//     }
 
-    plusLike(){
-        this._likes += 1;
-    }
+//     plusLike(){
+//         this._likes += 1;
+//     }
 
-    minesLike(){
-        this._likes -= 1;
-    }
+//     minesLike(){
+//         this._likes -= 1;
+//     }
 
-    addHashtag(hashtag) {
-        if (this._hashtags.length >= 6) {
-            console.log("Cannot add more than 6 hashtags.");
-        } else if (!ALLOWED_HASHTAGS.includes(hashtag)) {
-            console.log(`Hashtag '${hashtag}' is not allowed.`);
-        } else if (this._hashtags.includes(hashtag)) {
-            console.log(`Hashtag '${hashtag}' is already added.`);
-        } else {
-            this._hashtags.push(hashtag);
-        }
-    }
+//     addHashtag(hashtag) {
+//         if (this._hashtags.length >= 6) {
+//             console.log("Cannot add more than 6 hashtags.");
+//         } else if (!ALLOWED_HASHTAGS.includes(hashtag)) {
+//             console.log(`Hashtag '${hashtag}' is not allowed.`);
+//         } else if (this._hashtags.includes(hashtag)) {
+//             console.log(`Hashtag '${hashtag}' is already added.`);
+//         } else {
+//             this._hashtags.push(hashtag);
+//         }
+//     }
 
-    getHashtags() {
-        return this._hashtags;
-    }
+//     getHashtags() {
+//         return this._hashtags;
+//     }
 
-    render(){
-        document.write(`
-            <article class="post-cont">
-                <img class="post-photo" src="./assets/img/cat.jpg" alt="cat">
-                <h3 class="post-name">${this._postName}</h3>
-                <p class="autor">${this._author}</p>
-                <p class="post-text">${this._text}</p>
-                <p class="post-date">${this._date}</p>
-                <p class="post-likes">${this._likes} <span class="heart"><i class="fa-solid fa-heart"></i></span> <span class="share"><i class="fa-solid fa-share"></i></span></p>
-                <p class="post-hashtag">${this._hashtags.join(' ')}</p>
-            </article>
-        `);
-    };
-}
+//     render(){
+//         document.write(`
+//             <article class="post-cont">
+//                 <img class="post-photo" src="./assets/img/cat.jpg" alt="cat">
+//                 <h3 class="post-name">${this._postName}</h3>
+//                 <p class="autor">${this._author}</p>
+//                 <p class="post-text">${this._text}</p>
+//                 <p class="post-date">${this._date}</p>
+//                 <p class="post-likes">${this._likes} <span class="heart"><i class="fa-solid fa-heart"></i></span> <span class="share"><i class="fa-solid fa-share"></i></span></p>
+//                 <p class="post-hashtag">${this._hashtags.join(' ')}</p>
+//             </article>
+//         `);
+//     };
+// }
 
-const post1 = new Post(
-    1,
-    "Kitty",
-    "Animal Planet",
-    "Fat cat jumping on the bed",
-    2022,
-    100,
-);
+// const post1 = new Post(
+//     1,
+//     "Kitty",
+//     "Animal Planet",
+//     "Fat cat jumping on the bed",
+//     2022,
+//     100,
+// );
 
-console.log(post1);
+// console.log(post1);
 
-// змінити текст поста на інший.
-post1.newText("Small cat smile)");
+// // змінити текст поста на інший.
+// post1.newText("Small cat smile)");
 
-//збільшити кількість вподобайок на 1 
-// зменшити кількість вподобайок на 1.
-post1.plusLike();
-post1.minesLike();
+// //збільшити кількість вподобайок на 1 
+// // зменшити кількість вподобайок на 1.
+// post1.plusLike();
+// post1.minesLike();
 
-// render для отримання розмітки для посту 
-// (*в методі деструктуризувати інформацію з this).
+// // render для отримання розмітки для посту 
+// // (*в методі деструктуризувати інформацію з this).
 
-post1.addHashtag('#cat');
-post1.addHashtag('#dfsafsdf');
-post1.addHashtag('#kitty');
-post1.render();
+// post1.addHashtag('#cat');
+// post1.addHashtag('#dfsafsdf');
+// post1.addHashtag('#kitty');
+// post1.render();
 
 
 
